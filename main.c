c9dc773c967911202b15d89c208c40ba9a7eaf15
mrbravogil <mrbravogil@gmail.com>
2026-01-12 12:32:56 +0100

    cambios en main.c

--- main.c
+++ main.c
@@ -4,14 +4,16 @@ int main()
 {
 	int fd;
 	char *line;
+	int i = 1;
 	fd = open("text.txt", O_RDONLY);
 	// creacion de un bucle para recorrer todo el texto
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
-		printf("%s", line);
+		printf("%d. Linea: %s", i,line);
 		free(line);
 		line = get_next_line(fd);
+		i++;
 	}
 	close(fd);
 	return (0);
